6410ca75259bcfd14157bc2373756a9017634ec2
Victor-Egas <victor.egas.fieet@gmail.com>
2022-01-18 23:44:00 -0500

    Clean Proyect

--- src/main/java/com/banca/model/Customer.java
+++ src/main/java/com/banca/model/Customer.java
@@ -32,47 +32,6 @@ package com.banca.model;
 		@Field(name="type")
 		private String type;
 
-		public String getId() {
-			return id;
-		}
-
-		public void setId(String id) {
-			this.id = id;
-		}
-
-		public String getFirstName() {
-			return firstName;
-		}
-
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-
-		public String getLastName() {
-			return lastName;
-		}
-
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-
-		public String getDocument() {
-			return document;
-		}
-
-		public void setDocument(String document) {
-			this.document = document;
-		}
-
-		public String getType() {
-			return type;
-		}
-
-		public void setType(String type) {
-			this.type = type;
-		}
-		
-		
 	}
 
 
